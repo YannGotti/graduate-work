5e02774deea3652a9fb9745882da58c440fbceb0
mironovv <1111111111aleksey46@gmail.com>
2024-02-29 20:11:33 +0300

    minify js files

--- src/static/js/main/draw_elements.js
+++ src/static/js/main/draw_elements.js
@@ -1,61 +1,17 @@
-function formatDate(isoDate) {
-    const date = new Date(isoDate);
-    const options = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric'
-    };
-
-    const formattedDate = new Intl.DateTimeFormat('ru-RU', options).format(date);
-    return formattedDate;
-}
-
-function drawMaterialTrackedUsers(material, stats = false){
-    let html = 
-    `
+function formatDate(a){a=new Date(a);return(new Intl.DateTimeFormat("ru-RU",{year:"numeric",month:"long",day:"numeric",hour:"numeric",minute:"numeric"})).format(a)}function drawMaterialTrackedUsers(a,c=!1){let b=`
     <div class="p-2 m-2" style="border-radius: 10px; background-color: rgba(0, 0, 0, 0.100);">
 
         <div class="hstack d-flex justify-content-between p-2">
 
             <div class="hstack gap-2">
-                <a href="/${material.user.name}"><img class="mt-2" src="${material.user.photo_profile}" style="object-fit: cover; width: 25px; height: 25px; border-radius: 10px;"></a>
-                <a href="/${material.user.name}" style="text-decoration: none;" class="text-h5-custom text-dark mt-1">${material.user.name}</a>
-                `
-
-                if (!stats) {
-                    html += '<h5 class="text-h6-custom me-1" style="color: rgb(71, 71, 71);">опубликовал материал</h5>';
-                }
-                else{
-                    html += `<h1 class="text-h6-custom">${formatDate(material.created_at)}</h1>`;
-                }
-
-                html += 
-                `
-
-
-            </div>
-
-    `
-
-    if (stats) {
-        html +=
-        `
+                <a href="/${a.user.name}"><img class="mt-2" src="${a.user.photo_profile}" style="object-fit: cover; width: 25px; height: 25px; border-radius: 10px;"></a>
+                <a href="/${a.user.name}" style="text-decoration: none;" class="text-h5-custom text-dark mt-1">${a.user.name}</a>
+                `;b=c?b+`<h1 class="text-h6-custom">${formatDate(a.created_at)}</h1>`:b+'<h5 class="text-h6-custom me-1" style="color: rgb(71, 71, 71);">\u043e\u043f\u0443\u0431\u043b\u0438\u043a\u043e\u0432\u0430\u043b \u043c\u0430\u0442\u0435\u0440\u0438\u0430\u043b</h5>';b+="\n\n\n            </div>\n\n    ";b=c?b+`
             <div class="hstack gap-2">
                 <img src="/static/image/star_blank.png" class="mb-2" style="width: 15px; height: 15px;" alt="">
-                <h3 class="text-h6-custom text-dark">${material.num_marks}</h3>
+                <h3 class="text-h6-custom text-dark">${a.num_marks}</h3>
             </div>
-        `;
-    }
-
-    else {
-        html += `<h1 class="text-h6-custom mt-2">${formatDate(material.created_at)}</h1>`;
-    }
-
-
-    html += 
-    `
+        `:b+`<h1 class="text-h6-custom mt-2">${formatDate(a.created_at)}</h1>`;return b+=`
             
         </div>
         
@@ -63,23 +19,20 @@ function drawMaterialTrackedUsers(material, stats = false){
 
         <div class="m-2 p-2" style="border-radius: 5px; background-color: rgba(83, 83, 83, 0.085);">
             <div class="hstack gap-2" >
-                <a href="/${material.user.name}/${material.name}"><img class="mt-1" src="${material.icon}" style="object-fit: cover; width: 25px; height: 25px; border-radius: 10px;"></a>
-                <a href="/${material.user.name}/${material.name}" style="text-decoration: none;" class="text-h5-custom text-success">${material.name}</a>
+                <a href="/${a.user.name}/${a.name}"><img class="mt-1" src="${a.icon}" style="object-fit: cover; width: 25px; height: 25px; border-radius: 10px;"></a>
+                <a href="/${a.user.name}/${a.name}" style="text-decoration: none;" class="text-h5-custom text-success">${a.name}</a>
             </div>
 
-            <h5 class="text-h6-custom m-1 mt-2" style="word-wrap: break-word;">${material.description}</h5>
+            <h5 class="text-h6-custom m-1 mt-2" style="word-wrap: break-word;">${a.description}</h5>
 
             <div class="hstack gap-2 m-1 mt-0">
 
-                <h5 class="text-h6-custom mt-2">${material.theme}</h5>
-                <a href="${material.source}" style="text-decoration: none;" class="text-h6-custom text-success">${material.source}</a>
+                <h5 class="text-h6-custom mt-2">${a.theme}</h5>
+                <a href="${a.source}" style="text-decoration: none;" class="text-h6-custom text-success">${a.source}</a>
             </div>
 
         </div>
 
 
     </div>
-    `;
-    
-    return html;
-}
+    `};
--- src/static/js/profile/draw_elements.js
+++ src/static/js/profile/draw_elements.js
@@ -1,102 +1,20 @@
-
-
-function drawMarkMaterials(data){
-
-    for (const mark of data) {
-        if (mark.is_tracked)
-        {
-
-            try {
-                const del_form = document.getElementById('del_form_' + mark.material_id);
-                del_form.style.setProperty('display', 'block', 'important');
-            } catch (error) {
-                
-            }
-            
-        }
-        else
-        {
-
-            try {
-                const create_form = document.getElementById('create_form_' + mark.material_id);
-                create_form.style.setProperty('display', 'block', 'important');
-            } catch (error) {
-                
-            }
-            
-        }
-
-        try {
-            const count_mark = document.getElementById('count_mark_' + mark.material_id);
-            count_mark.innerText = mark.count_mark;
-        } catch (error) {
-            
-        }
-
-        
-    }
-
-}
-
-function drawSearchMaterials(materials){
-    const title_materials_search = document.getElementById('title_materials_search');
-    const searched_list_materials = document.getElementById('searched_list_materials');
-
-    if (materials.length == 0){
-        title_materials_search.style.display = 'none';
-        searched_list_materials.innerHTML = ``;
-        return;
-    } 
-
-    title_materials_search.style.display = 'block';
-
-    searched_list_materials.innerHTML = ``;
-
-    for (const material of materials) {
-        searched_list_materials.innerHTML +=
-        `
-        <a href="/${material.username}/${material.name}/" class="d-flex justify-content-between search-material  p-1" style="text-decoration: none;">
+function drawMarkMaterials(c){for(const a of c){if(a.is_tracked)try{document.getElementById("del_form_"+a.material_id).style.setProperty("display","block","important")}catch(b){}else try{document.getElementById("create_form_"+a.material_id).style.setProperty("display","block","important")}catch(b){}try{document.getElementById("count_mark_"+a.material_id).innerText=a.count_mark}catch(b){}}}
+function drawSearchMaterials(c){const a=document.getElementById("title_materials_search"),b=document.getElementById("searched_list_materials");if(0==c.length)a.style.display="none",b.innerHTML="";else{a.style.display="block";b.innerHTML="";for(const d of c)b.innerHTML+=`
+        <a href="/${d.username}/${d.name}/" class="d-flex justify-content-between search-material  p-1" style="text-decoration: none;">
             <div class="hstack gap-2" >
-                <img src="/media/${material.icon}" style="width: 25px; height: 25px; border-radius: 100%;" alt="">
-                <h6 class="text-h6-custom text-dark mt-1" style="text-decoration: none;">${material.name}</h6>
+                <img src="/media/${d.icon}" style="width: 25px; height: 25px; border-radius: 100%;" alt="">
+                <h6 class="text-h6-custom text-dark mt-1" style="text-decoration: none;">${d.name}</h6>
             </div>
 
-            <h6 class="text-h6-custom text-dark m-2">@${material.username}</h6>
+            <h6 class="text-h6-custom text-dark m-2">@${d.username}</h6>
         </a>
-        `
-    }
-}
-
-function drawSearchUsers(users){
-    const title_materials_search = document.getElementById('title_users_search');
-    const searched_list_materials = document.getElementById('searched_list_users');
-
-    if (users.length == 0){
-        title_materials_search.style.display = 'none';
-        searched_list_materials.innerHTML = ``;
-        return;
-    } 
-
-    title_materials_search.style.display = 'block';
-
-    searched_list_materials.innerHTML = ``;
-
-    for (const user of users) {
-        searched_list_materials.innerHTML +=
-        `
-        <a href="/${user.name}/" class="d-flex justify-content-between search-material  p-1" style="text-decoration: none;">
+        `}}function drawSearchUsers(c){const a=document.getElementById("title_users_search"),b=document.getElementById("searched_list_users");if(0==c.length)a.style.display="none",b.innerHTML="";else{a.style.display="block";b.innerHTML="";for(const d of c)b.innerHTML+=`
+        <a href="/${d.name}/" class="d-flex justify-content-between search-material  p-1" style="text-decoration: none;">
             <div class="hstack gap-2" >
-                <img src="/media/${user.photo_profile}" style="width: 25px; height: 25px; border-radius: 100%;" alt="">
-                <h6 class="text-h6-custom text-dark mt-1" style="text-decoration: none;">${user.name}</h6>
+                <img src="/media/${d.photo_profile}" style="width: 25px; height: 25px; border-radius: 100%;" alt="">
+                <h6 class="text-h6-custom text-dark mt-1" style="text-decoration: none;">${d.name}</h6>
             </div>
 
-            <h6 class="text-h6-custom text-dark m-2">Перейти</h6>
+            <h6 class="text-h6-custom text-dark m-2">\u041f\u0435\u0440\u0435\u0439\u0442\u0438</h6>
         </a>
-        `
-    }
-}
-
-function drawSearchMaterialsUsers(materials, users){
-    drawSearchMaterials(materials);
-    drawSearchUsers(users);
-}
+        `}}function drawSearchMaterialsUsers(c,a){drawSearchMaterials(c);drawSearchUsers(a)};
